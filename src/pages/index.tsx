03329debc7ddb870195dd11de8c98f4680d0fdf5
Roman Liubushkin <rl@mobalyticshq.com>
2023-08-16 20:05:36 +0700

    Add footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -118,6 +118,19 @@ const Index: React.FC<PageProps<DataProps>> = () => {
                 </div>
             </section>
 
+            <footer className="flex justify-between items-center bg-stone-900 p-6 text-white">
+                <div className="text-sm text-stone-300">
+
+                    <p className="text-stone-300">
+                        © 2023 streamerinsights.org  All rights reserved.
+                    </p>
+                </div>
+                <div className="flex items-center gap-4">
+                    <span className="font-semibold text-stone-200">Contacts</span>
+                    <a href="#" className="text-stone-300 hover:text-stone-400 underline">E-mail</a>
+                    <a href="#" className="text-stone-300 hover:text-stone-400 underline">Discord</a>
+                </div>
+            </footer>
         </div>
     )
 }
