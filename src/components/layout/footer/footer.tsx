e79dbe818be52ff67652fce6fb50d85832920239
Roman Liubushkin <rl@mobalyticshq.com>
2023-08-17 18:50:21 +0700

    Adjust title

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const Footer: React.FC<Props> = () => {
     return (
-        <footer className="flex justify-between items-center bg-stone-900 p-6 text-white">
+        <footer className="flex justify-between items-center bg-stone-900 py-6 px-14 text-white">
             <div className="text-sm text-stone-300">
                 <p className="text-stone-300">
                     © 2023 streamerinsights.org All rights reserved.
@@ -14,8 +14,7 @@ export const Footer: React.FC<Props> = () => {
             </div>
             <div className="flex items-center gap-4">
                 <span className="font-semibold text-stone-200">Contacts</span>
-                <a href="#" className="text-stone-300 hover:text-stone-400 underline">E-mail</a>
-                <a href="#" className="text-stone-300 hover:text-stone-400 underline">Discord</a>
+                <a href="mailto:contacts@streamerinsights.org" className="text-stone-300 hover:text-stone-400 underline">E-mail</a>
             </div>
         </footer>
     );
